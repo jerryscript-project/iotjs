c7a5bd7a5eb58b34db036cc323c9b8a61fdedfaf
Ilyong Cho <ily.cho@samsung.com>
2015-05-15 16:00:20 +0900

    Implemented events native module
    IoT.js-DCO-1.0-Signed-off-by: Ilyong Cho ily.cho@samsung.com

---
+++ src/js/events.js
@@ -0,0 +1,74 @@
+/* Copyright 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+var util = require('util');
+
+
+function EventEmitter() {
+  this._events = {};
+};
+
+module.exports = EventEmitter;
+
+
+// TODO: using arguments instead of arg1, arg2.
+EventEmitter.prototype.emit = function(type, arg1, arg2) {
+  if (!this._events) {
+    this._events = {};
+  }
+
+  var handler = this._events[type];
+  if (util.isUndefined(handler)) {
+    return false;
+  } else if (util.isFunction(handler) || util.isObject(handler)) {
+    if (util.isFunction(handler)) {
+      handler.call(this, arg1, arg2);
+    } else {
+      listeners = handler;
+      for (i = 0; i < listeners.length; ++i) {
+        listeners[i].call(this, arg1, arg2);
+      }
+    }
+  }
+
+  return true;
+};
+
+
+EventEmitter.prototype.addListener = function(type, listener) {
+  if (!util.isFunction(listener)) {
+    throw new TypeError('linster must be a function');
+  }
+
+  if (!this._events) {
+    this._events = {};
+  }
+
+  if (!this._events[type]) {
+    this._events[type] = listener;
+  } else if (util.isObject(this._events[type])) {
+    this._events[type].push(listener);
+  } else {
+    this._events[type] = [this._events[type], listener];
+  }
+
+  return this;
+};
+
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+

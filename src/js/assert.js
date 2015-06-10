f0eadd6ba2ec52264466ef68f5f65638b40004c5
Ilyong Cho <ily.cho@samsung.com>
2015-06-10 15:57:30 +0900

    Implemented assert module
    IoT.js-DCO-1.0-Signed-off-by: Ilyong Cho ily.cho@samsung.com

---
+++ src/js/assert.js
@@ -0,0 +1,133 @@
+/* Copyright 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var util = require('util');
+
+
+function AssertionError(options) {
+  this.name = 'AssertionError';
+  this.actual = options.actual;
+  this.expected = options.expected;
+  this.operator = options.operator;
+  if (options.message) {
+    this.message = options.message;
+  } else {
+    this.message = getMessage(this);
+  }
+}
+
+util.inherits(AssertionError, Error);
+
+
+function getMessage(assertion) {
+  // FIXME: use `JSON.stringify` to generate assertion message.
+  return 'AssertionError';
+}
+
+
+function assert(value, message) {
+  if (!value) {
+    fail(value, true, message, '==');
+  }
+}
+
+
+function fail(actual, expected, message, operator) {
+  throw new AssertionError({
+    message: message,
+    actual: actual,
+    expected: expected,
+    operator: operator
+  });
+}
+
+
+function equal(actual, expected, message) {
+  if (actual != expected) {
+    fail(actual, expected, message, '==');
+  }
+}
+
+
+function notEqual(actual, expected, message) {
+  if (actual == expected) {
+    fail(actual, expected, message, '!=');
+  }
+}
+
+
+function strictEqual(actual, expected, message) {
+  if (actual !== expected) {
+    fail(actual, expected, message, '===');
+  }
+}
+
+
+function notStrictEqual(actual, expected, message) {
+  if (actual === expected) {
+    fail(actual, expected, message, '!==');
+  }
+}
+
+
+function throws(block, expected, message) {
+  var actual;
+
+  try {
+    block();
+  } catch (e) {
+    actual = e;
+  }
+
+  message = (expected && expected.name ? '(' + expected.name + ').' : '.') +
+            (message ? ' ' + message : '.');
+
+  if (!actual) {
+    fail(actual, expected, 'Missing expected exception' + message);
+  }
+  if (!(actual instanceof expected)) {
+    throw actual;
+  }
+}
+
+
+function doesNotThrow(block, message) {
+  var actual;
+
+  try {
+    block();
+  } catch (e) {
+    actual = e;
+  }
+
+  message = (message ? ' ' + message : '');
+
+  if (actual) {
+    fail(actual, null, 'Got unwanted exception.' + message);
+  }
+}
+
+
+assert.AssertionError = AssertionError;
+assert.assert = assert;
+assert.fail = fail;
+assert.equal = equal;
+assert.notEqual = notEqual;
+assert.strictEqual = strictEqual;
+assert.notStrictEqual = notStrictEqual;
+assert.throws = throws;
+assert.doesNotThrow = doesNotThrow;
+
+module.exports = assert;
---
+++ test/test_assert.js
@@ -0,0 +1,89 @@
+/* Copyright 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var assert = require('assert');
+
+
+assert.assert(1 == 1);
+
+assert.equal(1, 1);
+assert.notEqual(1, 2);
+
+assert.strictEqual(0, 0);
+
+assert.equal(0, false);
+assert.notStrictEqual(0, false);
+
+assert.throws(
+  function() {
+    assert.equal(1, 2);
+  },
+  assert.AssertionError
+);
+
+assert.throws(
+  function() {
+    assert.assert(1 == 2);
+  },
+  assert.AssertionError
+);
+
+assert.doesNotThrow(
+  function() {
+    assert.assert(1 == 1);
+  }
+);
+
+assert.throws(
+  function() {
+    assert.doesNotThrow(
+      function() {
+        assert.assert(1 == 2);
+      }
+    );
+  },
+  assert.AssertionError
+);
+
+try {
+  assert.assert(false, 'assert test');
+} catch (e) {
+  assert.equal(e.name, 'AssertionError');
+  assert.equal(e.actual, false);
+  assert.equal(e.expected, true);
+  assert.equal(e.operator, '==');
+  assert.equal(e.message, 'assert test');
+}
+
+try {
+  assert.equal(1, 2, 'assert.equal test');
+} catch (e) {
+  assert.equal(e.name, 'AssertionError');
+  assert.equal(e.actual, 1);
+  assert.equal(e.expected, 2);
+  assert.equal(e.operator, '==');
+  assert.equal(e.message, 'assert.equal test');
+}
+
+
+try {
+  assert.fail('actual', 'expected', 'message', 'operator');
+} catch (e) {
+  assert.equal(e.name, 'AssertionError');
+  assert.equal(e.actual, 'actual');
+  assert.equal(e.expected, 'expected');
+  assert.equal(e.operator, 'operator');
+  assert.equal(e.message, 'message');
+}

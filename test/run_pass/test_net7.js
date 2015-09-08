4d3031fee32d889a14cf802254a782ac166aaeaf
Ilyong Cho <ily.cho@samsung.com>
2015-09-08 10:51:42 +0900

    Added a test for stablizing net module. is marked as skip.
    IoT.js-DCO-1.0-Signed-off-by: Ilyong Cho ily.cho@samsung.com

---
+++ test/run_pass/test_net7.js
@@ -0,0 +1,80 @@
+/* Copyright 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /*
+   @SKIP
+ */
+
+
+var net = require('net');
+var assert = require('assert');
+var timers = require('timers');
+var stream = require('stream');
+
+var port = 9827;
+
+var count = 50;
+var check = [];
+
+function serverListen() {
+  var server = net.createServer({
+    allowHalfOpen: true
+  });
+
+  var cnt = 0;
+
+  server.listen(port);
+
+  server.on('connection', function(socket) {
+    var msg = '';
+    socket.on('data', function(data) {
+      msg += data;
+    });
+    socket.on('end', function() {
+      socket.end(msg);
+      cnt++;
+
+      if (cnt == count) {
+        server.close();
+      }
+    });
+  });
+}
+
+serverListen();
+
+for (var i = 0; i < count; ++i) {
+  (function(i) {
+    var socket = new net.Socket();
+    var msg = "";
+
+    socket.connect(port, "localhost");
+    socket.on('connect', function() {
+      socket.end(i.toString());
+    });
+    socket.on('data', function(data) {
+      check[data] = true;
+    });
+  })(i);
+}
+
+process.on('exit', function(code) {
+  assert.equal(code, 0);
+  for (var i = 0; i < count; ++i) {
+    if (!check[i]) {
+      assert.fail();
+    }
+  }
+});

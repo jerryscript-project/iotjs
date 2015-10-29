cdfbb2e3fa5994c4792a667735f05e66d5f18a4a
chunseok lee <chunseok.lee@samsung.com>
2015-10-29 17:22:10 +0900

    gpio for nuttx
    10 gpios from PA8 to PC3 on default
    
    IoT.js-DCO-1.0-Signed-off-by: Chunseok Lee chunseok.lee@samsung.com

--- src/platform/arm-nuttx/iotjs_module_gpio-arm-nuttx.cpp
+++ src/platform/arm-nuttx/iotjs_module_gpio-arm-nuttx.cpp
@@ -18,6 +18,38 @@
 
 #include "iotjs_module_gpio.h"
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <nuttx/gpio.h>
+#include <sys/ioctl.h>
+
+
+#define GPIO_MAX_PINNO 10
+
+
+
+// NUTTX on STM32F4 Gpio map in IoT.js //
+
+uint32_t gpioPin[GPIO_MAX_PINNO+1] = {
+  0,
+  (GPIO_PIN8   | GPIO_PORTA),
+  (GPIO_PIN10  | GPIO_PORTA ),
+  (GPIO_PIN15  | GPIO_PORTA ),
+  (GPIO_PIN11  | GPIO_PORTD ),
+  (GPIO_PIN3   | GPIO_PORTA ),
+  (GPIO_PIN5   | GPIO_PORTA ),
+  (GPIO_PIN14  | GPIO_PORTB ),
+  (GPIO_PIN10  | GPIO_PORTB ),
+  (GPIO_PIN2   | GPIO_PORTC ),
+  (GPIO_PIN3   | GPIO_PORTC ),
+};
+
+uint32_t gpioDirection[3] = {
+  (GPIO_OUTPUT | GPIO_FLOAT),
+  (GPIO_INPUT  | GPIO_PULLDOWN),
+  (GPIO_OUTPUT | GPIO_PULLUP) };
+
+
 
 namespace iotjs {
 
@@ -34,6 +66,9 @@ class GpioArmNuttx : public Gpio {
   virtual int SetPin(GpioReqWrap* gpio_req);
   virtual int WritePin(GpioReqWrap* gpio_req);
   virtual int ReadPin(GpioReqWrap* gpio_req);
+
+  bool _initialized;
+  int _fd; // ioctl handle for gpio on nuttx
 };
 
 
@@ -45,6 +80,8 @@ Gpio* Gpio::Create(JObject& jgpio) {
 
 GpioArmNuttx::GpioArmNuttx(JObject& jgpio)
     : Gpio(jgpio) {
+  _fd = NULL;
+  _initialized = false;
 }
 
 
@@ -54,33 +91,209 @@ GpioArmNuttx* GpioArmNuttx::GetInstance()
 }
 
 
+void AfterWork(uv_work_t* work_req, int status) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  if (status) {
+    req_data->result = kGpioErrSys;
+  }
+
+  JArgList jargs(2);
+  jargs.Add(JVal::Number(req_data->result));
+
+  switch (req_data->op) {
+    case kGpioOpInitize:
+    {
+      if (req_data->result == kGpioErrOk) {
+        gpio->_initialized = true;
+      }
+      break;
+    }
+    case kGpioOpRelease:
+    {
+      if (req_data->result == kGpioErrOk) {
+        gpio->_initialized = false;
+      }
+      break;
+    }
+    case kGpioOpSetPin:
+    case kGpioOpWritePin:
+    {
+      break;
+    }
+    case kGpioOpReadPin:
+    {
+      if (req_data->result == kGpioErrOk) {
+        jargs.Add(JVal::Bool(req_data->value));
+      }
+      break;
+    }
+
+    case kGpioOpSetPort:
+    case kGpioOpWritePort:
+    case kGpioOpReadPort:
+    {
+      IOTJS_ASSERT(!"Not implemented");
+      break;
+    }
+    default:
+    {
+      IOTJS_ASSERT(!"Unreachable");
+      break;
+    }
+  }
+
+  MakeCallback(gpio_req->jcallback(), *Gpio::GetJGpio(), jargs);
+
+  delete work_req;
+  delete gpio_req;
+}
+
+
+void InitializeWorker(uv_work_t* work_req) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+  IOTJS_ASSERT(gpio->_initialized == false);
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  DDDLOG("GPIO InitializeWorker()");
+
+  const char* devfilepath = "/dev/gpio";
+  gpio->_fd = open(devfilepath, O_RDWR);
+  DDDLOG("gpio> %s : fd(%d)", devfilepath, gpio->_fd);
+
+  // Check if GPIO handle is OK.
+  if (gpio->_fd) {
+    req_data->result = kGpioErrOk;
+  } else {
+    req_data->result = kGpioErrInitialize;
+  }
+}
+
+
+void ReleaseWorker(uv_work_t* work_req) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+  IOTJS_ASSERT(gpio->_initialized == true);
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  DDDLOG("GPIO ReleaseWorker()");
+
+  if (gpio->_fd) {
+    close(gpio->_fd);
+  }
+  req_data->result = kGpioErrOk;
+}
+
+
+void SetPinWorker(uv_work_t* work_req) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+  IOTJS_ASSERT(gpio->_initialized == true);
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  DDDLOG("GPIO SetPinWorker() - pin: %d, dir: %d, mode: %d",
+         req_data->pin, req_data->dir, req_data->mode);
+  struct gpioioctl_config_s cdata;
+  cdata.port = (gpioPin[req_data->pin] | gpioDirection[req_data->dir]);
+
+  if (!ioctl(gpio->_fd, GPIOIOCTL_CONFIG, (long unsigned int)&cdata)) {
+    req_data->result = kGpioErrOk;
+  } else {
+    req_data->result = kGpioErrSys;
+  }
+}
+
+
+
+void WritePinWorker(uv_work_t* work_req) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+  IOTJS_ASSERT(gpio->_initialized == true);
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  DDDLOG("GPIO WritePinWorker() - pin: %d, value: %d",
+         req_data->pin, req_data->value);
+
+  struct gpioioctl_write_s wdata;
+  wdata.port = gpioPin[req_data->pin];
+  wdata.data = req_data->value;
+
+  if (!ioctl(gpio->_fd, GPIOIOCTL_WRITE, (long unsigned int)&wdata)) {
+    req_data->result = kGpioErrOk;
+  } else {
+    req_data->result = kGpioErrSys;
+  }
+}
+
+
+
+void ReadPinWorker(uv_work_t* work_req) {
+  GpioArmNuttx* gpio = GpioArmNuttx::GetInstance();
+  IOTJS_ASSERT(gpio->_initialized == true);
+
+  GpioReqWrap* gpio_req = reinterpret_cast<GpioReqWrap*>(work_req->data);
+  GpioReqData* req_data = gpio_req->req();
+
+  DDDLOG("GPIO ReadPinWorker() - pin: %d" ,req_data->pin);
+
+  struct gpioioctl_read_s rdata;
+  int ret;
+  rdata.port = gpioPin[req_data->pin];
+
+  if (!ioctl(gpio->_fd, GPIOIOCTL_READ, (long unsigned int)&rdata)) {
+    req_data->value = rdata.data;
+    req_data->result = kGpioErrOk;
+  } else {
+    req_data->result = kGpioErrSys;
+  }
+}
+
+
+#define GPIO_NUTTX_IMPL_TEMPLATE(op, initialized) \
+  do { \
+    GpioArmNuttx* gpio = GpioArmNuttx::GetInstance(); \
+    IOTJS_ASSERT(gpio->_initialized == initialized); \
+    Environment* env = Environment::GetEnv(); \
+    uv_work_t* req = new uv_work_t; \
+    req->data = reinterpret_cast<void*>(gpio_req); \
+    uv_queue_work(env->loop(), req, op ## Worker, AfterWork); \
+  } while (0)
+
 
 int GpioArmNuttx::Initialize(GpioReqWrap* gpio_req) {
-  IOTJS_ASSERT(!"Not implemented");
+  GPIO_NUTTX_IMPL_TEMPLATE(Initialize, false);
   return 0;
 }
 
 
 int GpioArmNuttx::Release(GpioReqWrap* gpio_req) {
-  IOTJS_ASSERT(!"Not implemented");
+  GPIO_NUTTX_IMPL_TEMPLATE(Release, true);
   return 0;
 }
 
 
 int GpioArmNuttx::SetPin(GpioReqWrap* gpio_req) {
-  IOTJS_ASSERT(!"Not implemented");
+  GPIO_NUTTX_IMPL_TEMPLATE(SetPin, true);
   return 0;
 }
 
 
 int GpioArmNuttx::WritePin(GpioReqWrap* gpio_req) {
-  IOTJS_ASSERT(!"Not implemented");
+  GPIO_NUTTX_IMPL_TEMPLATE(WritePin, true);
   return 0;
 }
 
 
 int GpioArmNuttx::ReadPin(GpioReqWrap* gpio_req) {
-  IOTJS_ASSERT(!"Not implemented");
+  GPIO_NUTTX_IMPL_TEMPLATE(ReadPin, true);
   return 0;
 }
 

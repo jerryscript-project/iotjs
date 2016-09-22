20aac30283ef52c6ae7415f61a6e13424b173da8
Youngil Choi <duddlf.choi@samsung.com>
2016-09-22 17:07:56 +0900

    Correct tidy and license check
    IoT.js-DCO-1.0-Signed-off-by: Youngil Choi duddlf.choi@samsung.com

--- tools/check_license.py
+++ tools/check_license.py
@@ -17,19 +17,19 @@
 import re;
 
 license = re.compile(
-u"""(#|//|\*) Copyright \d{4}(-\d{4})? Samsung Electronics Co., Ltd.
-\s?\\1
-\s?\\1 Licensed under the Apache License, Version 2.0 \(the "License"\);
-\s?\\1 you may not use this file except in compliance with the License.
-\s?\\1 You may obtain a copy of the License at
-\s?\\1
-\s?\\1     http://www.apache.org/licenses/LICENSE-2.0
-\s?\\1
-\s?\\1 Unless required by applicable law or agreed to in writing, software
-\s?\\1 distributed under the License is distributed on an "AS IS" BASIS
-\s?\\1 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-\s?\\1 See the License for the specific language governing permissions and
-\s?\\1 limitations under the License.""")
+u"""((#|//|\*) Copyright .*
+)+\s?\\2
+\s?\\2 Licensed under the Apache License, Version 2.0 \(the "License"\);
+\s?\\2 you may not use this file except in compliance with the License.
+\s?\\2 You may obtain a copy of the License at
+\s?\\2
+\s?\\2     http://www.apache.org/licenses/LICENSE-2.0
+\s?\\2
+\s?\\2 Unless required by applicable law or agreed to in writing, software
+\s?\\2 distributed under the License is distributed on an "AS IS" BASIS
+\s?\\2 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+\s?\\2 See the License for the specific language governing permissions and
+\s?\\2 limitations under the License.""")
 
 
 def check_license(contents):

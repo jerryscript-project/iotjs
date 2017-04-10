0c6e683000df06d00457f7f6154dd1e71d7458ef
László Langó <llango.u-szeged@partner.samsung.com>
2017-04-11 01:46:52 +0200

    Add input validation to 'iotjs_jval_create_string' (#727)
    Fixes #726
    
    IoT.js-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- test/run_pass/test_buffer.js
+++ test/run_pass/test_buffer.js
@@ -143,3 +143,6 @@ assert.equal(buff16.readInt8(3), 13);
 assert.equal(Buffer(new Array()).toString(), '');
 assert.equal(new Buffer(1).readUInt8(1, true), 0);
 assert.equal(new Buffer(1).readUInt16LE({}, true), 0);
+
+var buff17 = new Buffer("a");
+assert.throws(function() { buff17.fill(8071).toString(); }, TypeError);

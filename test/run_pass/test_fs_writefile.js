01e6028a800f3442d7cf7a35287a7758c6298977
Krzysztof Antoszek <k.antoszek@samsung.com>
2017-05-14 10:46:08 +0200

    FileSystem fix: writeFile/writeFileSync now accept string values as data
    IoT.js-DCO-1.0-Signed-off-by: Krzysztof Antoszek k.antoszek@samsung.com

---
+++ test/run_pass/test_fs_writefile.js
@@ -0,0 +1,50 @@
+/* Copyright 2016-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /*
+  @STDOUT=Pass
+ */
+
+var fs = require('fs');
+var assert = require('assert');
+
+var file = process.cwd() + '/resources/test';
+var buff1 = new Buffer('test string1');
+var str = 'test string2';
+var num = 1;
+
+fs.writeFile(file, buff1, function (err) {
+  assert.equal(err, null);
+  fs.readFile(file, function (err, buff2) {
+    assert.equal(err, null);
+      assert.equal(buff2.equals(buff1), true);
+      fs.writeFile(file, str, function (err) {
+        assert.equal(err, null);
+        fs.readFile(file, function (err, buff2) {
+          assert.equal(err, null);
+          assert.equal(str.valueOf(), buff2.toString('utf8'));
+            fs.writeFile(file, num, function (err) {
+            assert.equal(err, null);
+              fs.readFile(file, function (err, buff2) {
+                assert.equal(err, null);
+                assert.equal(num, parseInt(buff2.toString('utf8'), 10));
+                console.log('Pass');
+                fs.unlinkSync(file);
+              });
+            });
+        });
+      });
+    });
+});

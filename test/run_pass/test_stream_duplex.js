4fc48719a233bd0e513530aa8073611dcd9b6048
Daeyeon Jeong <daeyeon.dev@gmail.com>
2017-06-19 06:48:22 +0900

    Add a test for Duplex (#953)
    IoT.js-DCO-1.0-Signed-off-by: Daeyeon Jeong daeyeon.jeong@samsung.com

--- test/run_pass/test_stream_duplex.js
+++ test/run_pass/test_stream_duplex.js
@@ -13,8 +13,26 @@
  * limitations under the License.
  */
 
- var assert = require('assert');
+var assert = require('assert');
+var Duplex = require('stream').Duplex;
 
- var Duplex = require('stream').Duplex;
+var duplex = Duplex({ highWaterMark: 0 });
 
- var duplex = Duplex({highWaterMark: 0});
+assert(duplex instanceof Duplex);
+assert(duplex.write);
+assert(duplex.read);
+
+var msg1 = 'message 1';
+var msg2 = 'message 2';
+
+duplex._write = function(chunk, callback) {
+  assert.equal(chunk == msg1, true);
+  duplex.push(msg2);
+  duplex.end();
+}
+duplex._readyToWrite();
+duplex.write(msg1);
+
+process.on('exit', function() {
+  assert.equal(duplex.read() == msg2, true);
+});

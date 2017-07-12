e881f6981aecea2f2598f3143f70be0cc267785d
sumin.lim <sumin.lim@samsung.com>
2017-07-12 16:59:50 +0900

    Support Tizen GBS build (#1028)
    IoT.js-DCO-1.0-Signed-off-by: Sumin Lim sumin.lim@samsung.com

---
+++ config/tizen/gbsbuild.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Copyright 2017-present Samsung Electronics Co., Ltd. and other contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd ..
+
+echo "******************************************************************"
+echo "*                       Tizen GBS build                          *"
+echo "*                                                                *"
+echo "* Please input user, passwd of http://Tizen.org on '~/.gbs.conf' *"
+echo "* For more information, please read Guide Docs. folder           *"
+echo "* ~/.gbs.conf sample is at 'config/tizen/sample.gbs.conf'.       *"
+echo "*                                                                *"
+echo "******************************************************************"
+read -p "[Warning] This working folder will be copied to ../iotjs_tizen_gbs \
+Are you sure to continue? (y/n) " -n 1 -r
+
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo copy from $OLDPWD to ../iotjs_tizen_gbs
+  cp -ra $OLDPWD iotjs_tizen_gbs
+  cd iotjs_tizen_gbs
+  echo -e "\n(1) Now, cloning submodules. "
+  git submodule init
+  echo -e "\n(2) Update submodules... "
+  git submodule update
+  echo -e "\n(3) Remove compiler specified lines on CMake"
+  rm ./cmake/config/arm-tizen.cmake
+  echo "include(CMakeForceCompiler)
+
+  set(CMAKE_SYSTEM_NAME Linux)
+  set(CMAKE_SYSTEM_PROCESSOR armv7l)"\
+   >> ./cmake/config/arm-tizen.cmake
+
+  rm ./deps/libtuv/cmake/config/config_arm-tizen.cmake
+  echo "include(CMakeForceCompiler)
+
+  set(CMAKE_SYSTEM_NAME Linux)
+  set(CMAKE_SYSTEM_PROCESSOR arm)"\
+   >> ./deps/libtuv/cmake/config/config_arm-tizen.cmake
+
+  rm ./deps/jerry/cmake/toolchain_linux_armv7l-el.cmake
+  echo "set(CMAKE_SYSTEM_NAME Linux)
+  set(CMAKE_SYSTEM_PROCESSOR armv7l-el)
+  set(FLAGS_COMMON_ARCH -mlittle-endian -mthumb)" \
+   >> ./deps/jerry/cmake/toolchain_linux_armv7l-el.cmake
+
+  echo -e "\n(4) 3 files have been modified."
+
+  find ./ -name '.git' | xargs rm -rf
+  # Initialize Git repositoryㅣ
+  if [ ! -d .git ]
+  then
+    git init ./
+    git checkout -b tizen_gbs
+    git add ./
+    git commit -m "Initial commit"
+  fi
+
+  echo -e "\n(5) Calling core gbs build command"
+  gbscommand="gbs build -A armv7l --include-all --clean"
+  echo $gbscommand
+  if eval $gbscommand
+  then
+    echo "========================================================"
+    echo "1. GBS Build is successful."
+    echo "2. Please move to new working folder ../iotjs_tizen_gbs "
+    echo "   cd ../iotjs_tizen_gbs"
+    echo "3. From now, you can build with this command on new directory"
+    echo "   gbs build -A armv7l --include"
+    echo "4. Your new branch 'tizen_gbs' is added."
+    echo "5. 'iotjs origin' repository is added."
+    git remote add origin https://github.com/samsung/iotjs
+    echo "(You cant fetch origin repository with this command)"
+    echo "   git fetch --all"
+    echo "========================================================"
+    # git remote add origin
+    # https://review.tizen.org/git/platform/upstream/iotjs
+    git branch -a
+    git status
+  else
+    echo "GBS Build failed!"
+    exit 1
+  fi
+fi
--- docs/build/Build-for-ARTIK10-Tizen.md
+++ docs/build/Build-for-ARTIK10-Tizen.md
@@ -1,20 +1,25 @@
 
-### Tizen on ARTIK10 cross-compile
+### 1. Tizen on ARTIK10 cross-compile
 
 #### Prerequisites
 
-* ARTIK10 with Tizen (https://wiki.tizen.org/wiki/Tizen_On_ARTIK)
-* Tizen Studio with Native app development CLI tools
-  
-  This is required to get rootstrap for Tizen (set of native libraries).
-* arm-linux-gnueabi-gcc cross compiler (can be found in Tizen Studio / Native toolchain)
+* ARTIK10 with Tizen (https://wiki.tizen.org/wiki/Tizen_On_ARTIK)  
+* Tizen Studio with Native app development CLI tools.  
+  This is required to get rootstrap for Tizen (set of native libraries).  
+
+* arm-linux-gnueabi-gcc cross compiler (can be found in Tizen Studio / Native toolchain)  
+  Otherwise, you can install it on your PC.  
+```bash
+sudo apt-get install gcc-arm-linux-gnueabi g++-arm-linux-gnueabi
+```
 
 #### Building
-1. Make sure arm-linux-gnueabi-gcc is in path
-2. Locate Tizen SDK. Default location is: ~/tizen-studio
-3. In platforms/tizen-3.0/mobile there should be compatible rootstrap (eg. mobile-3.0-device)
+1. Make sure arm-linux-gnueabi-gcc is in path. 
+2. Locate Tizen SDK. Default location is: ~/tizen-studio.  
+3. In platforms/tizen-3.0/mobile there should be compatible rootstrap (eg. mobile-3.0-device)  
 
 Compile:
+* Compile with rootstrap in case you use tizen 3.0 libraries.  
 ``` bash
 tools/build.py \
   --target-arch=arm --target-os=tizen --target-board=artik10 \
@@ -22,7 +27,7 @@ tools/build.py \
 ```
 
 #### Testing
-Transfer iotjs binary and test file to the device:
+Transfer iotjs binary and test file to the device:  
 ``` bash
 sdb push ./build/arm-tizen/debug/bin/iotjs /home/owner/iotjs/
 sdb push ./test/run_pass/test_console.js /home/owner/iotjs/
@@ -35,3 +40,51 @@ $ cd /home/owner/iotjs
 $ ./iotjs test_console.js
 ```
 
+
+### 2. Build with GBS
+
+#### Prerequisites
+* Tizen uses GBS to create RPM packages.  
+  SDB tool is in Tizen Studio. To send a file, please, install tizen studio.  
+  (https://developer.tizen.org/development/tizen-studio/download)  
+* To run GBS, please create a GBS configuration file at '~/.gbs.conf'  
+ (https://source.tizen.org/documentation/reference/git-build-system/configuration-file)  
+ You can use this sample, /config/tizen/sample.gbs.conf for GBS build.  
+``` bash
+sudo apt-get install gbs mic
+cp ./config/tizen/sample.gbs.conf ~/.gbs.conf
+```  
+Please add your Tizen.org id and password on this conf file.  
+
+#### Building
+* You can modify IoT.js build option on the spec file.  
+ (config/tizen/packaging/iotjs.spec)  
+* Run gbsbuild.sh at first.
+Compile:
+``` bash
+cp ./config/tizen/gbsbuild.sh ./
+./gbsbuild.sh
+```
+After finishing build, move to a new working directory at '../iotjs_tizen_gbs/'.  
+Next time, build with this basic command.
+```bash
+gbs build -A armv7l --include-all
+```
+
+#### Install
+Transfer iotjs binary and test file to the device:
+``` bash
+sdb push  ~/GBS-ROOT/local/repos/tizen_unified/armv7l/RPMS/iotjs-1.0.0-0.armv7l.rpm /tmp
+sdb push ./test/run_pass/test_console.js /home/owner/iotjs/
+sdb root on
+sdb shell
+(target)$ cd /tmp
+(only in headless Tizen 4.0 target)$ mount -o remount,rw
+(target)$ rpm -ivh --force iotjs-1.0.0.rpm
+```
+
+Run the test:
+``` bash
+sdb shell
+$ iotjs test_console.js
+```

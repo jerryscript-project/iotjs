251976e73aa652cfdd99f552a7715b2fe3ccbec2
Akhil Kedia <akhilkedia94@gmail.com>
2017-08-28 12:18:18 +0900

    Fix to remove dots from module path before caching. (#1119)
    Previously, several paths pointing to the same module could be cached
    by module.js as it did not realize the absolute paths for them were the same.
    
    This resulted in Significantly higher memory usage as the same module was
    loaded repeatedly if 'required' by files in different folders.
    
    IoT.js-DCO-1.0-Signed-off-by: Akhil Kedia akhil.kedia@samsung.com

--- src/js/module.js
+++ src/js/module.js
@@ -33,25 +33,27 @@ iotjs_module_t.wrap = Native.wrap;
 
 
 var cwd;
-try { cwd = process.cwd(); } catch (e) { }
+try {
+  cwd = process.cwd();
+} catch (e) { }
 
-var moduledirs = [""]
-if(cwd){
+var moduledirs = [""];
+if (cwd) {
   moduledirs.push(cwd + "/");
   moduledirs.push(cwd + "/iotjs_modules/");
 }
-if(process.env.HOME){
+if (process.env.HOME) {
   moduledirs.push(process.env.HOME + "/iotjs_modules/");
 }
-if(process.env.IOTJS_PATH){
-  moduledirs.push(process.env.IOTJS_PATH + "/iotjs_modules/")
+if (process.env.IOTJS_PATH) {
+  moduledirs.push(process.env.IOTJS_PATH + "/iotjs_modules/");
 }
 
 
 iotjs_module_t.resolveDirectories = function(id, parent) {
   var dirs = moduledirs;
-  if(parent) {
-    if(!parent.dirs){
+  if (parent) {
+    if (!parent.dirs) {
       parent.dirs = [];
     }
     dirs = parent.dirs.concat(dirs);
@@ -61,8 +63,7 @@ iotjs_module_t.resolveDirectories = function(id, parent) {
 
 
 iotjs_module_t.resolveFilepath = function(id, directories) {
-
-  for(var i = 0; i<directories.length ; i++) {
+  for (var i = 0; i < directories.length; i++) {
     var dir = directories[i];
     var modulePath = dir + id;
 
@@ -76,30 +77,30 @@ iotjs_module_t.resolveFilepath = function(id, directories) {
     // 1. 'id'
     var filepath = iotjs_module_t.tryPath(modulePath);
 
-    if(filepath){
+    if (filepath) {
       return filepath;
     }
 
     // 2. 'id.js'
     filepath = iotjs_module_t.tryPath(modulePath + '.js');
 
-    if(filepath){
+    if (filepath) {
       return filepath;
     }
 
     // 3. package path id/
     var jsonpath = modulePath + "/package.json";
     filepath = iotjs_module_t.tryPath(jsonpath);
-    if(filepath){
+    if (filepath) {
       var pkgSrc = process.readSource(jsonpath);
       var pkgMainFile = JSON.parse(pkgSrc).main;
       filepath = iotjs_module_t.tryPath(modulePath + "/" + pkgMainFile);
-      if(filepath){
+      if (filepath) {
         return filepath;
       }
       // index.js
       filepath = iotjs_module_t.tryPath(modulePath + "/" + "index.js");
-      if(filepath){
+      if (filepath) {
         return filepath;
       }
     }
@@ -120,28 +121,56 @@ iotjs_module_t.resolveModPath = function(id, parent) {
 
   var filepath = iotjs_module_t.resolveFilepath(id, directories);
 
-  if(filepath){
-    return filepath;
+  if (filepath) {
+    return iotjs_module_t.normalizePath(filepath);
   }
 
   return false;
 };
 
 
+iotjs_module_t.normalizePath = function(path) {
+  var beginning = '';
+  if (path.indexOf('/') === 0) {
+    beginning = '/';
+  }
+
+  var input = path.split('/');
+  var output = [];
+  while (input.length > 0) {
+    if (input[0] === '.' || (input[0] === '' && input.length > 1)) {
+      input.shift();
+      continue;
+    }
+    if (input[0] === '..') {
+      input.shift();
+      if (output.length > 0 && output[output.length - 1] !== '..') {
+        output.pop();
+      } else {
+        throw new Error('Requested path is below root: ' + path);
+      }
+      continue;
+    }
+    output.push(input.shift());
+  }
+  return beginning + output.join('/');
+};
+
+
 iotjs_module_t.tryPath = function(path) {
   try {
     var stats = fs.statSync(path);
-    if(stats && !stats.isDirectory()) {
+    if (stats && !stats.isDirectory()) {
       return path;
     }
-  } catch (ex) {}
+  } catch (ex) { }
 
   return false;
 };
 
 
 iotjs_module_t.load = function(id, parent, isMain) {
-  if(process.native_sources[id]){
+  if (process.native_sources[id]) {
     return Native.require(id);
   }
   var module = new iotjs_module_t(id, parent);
@@ -174,9 +203,9 @@ iotjs_module_t.prototype.compile = function() {
 };
 
 
-iotjs_module_t.runMain = function(){
+iotjs_module_t.runMain = function() {
   iotjs_module_t.load(process.argv[1], null, true);
-  while(process._onNextTick());
+  while (process._onNextTick());
 };
 
 iotjs_module_t.prototype.require = function(id) {

025d76cea1f4667965946cb433bb29ca613e1f50
Krzysztof Antoszek <k.antoszek@samsung.com>
2017-08-30 16:41:42 +0200

    Fix DNS lookup handling for TizenRT
    IoT.js-DCO-1.0-Signed-off-by: Krzysztof Antoszek k.antoszek@samsung.com

--- test/run_pass/test_dns_lookup.js
+++ test/run_pass/test_dns_lookup.js
@@ -54,13 +54,11 @@ dns.lookup('localhost', function(err, ip, family) {
 // Test with invalid hostname.
 dns.lookup('invalid', 4, function(err, ip, family) {
   assert.notEqual(err, null);
-  assert.equal(err.code == -3008 || err.code == -3007, true);
 });
 
 // Test with empty hostname.
 dns.lookup('', 4, function(err, ip, family) {
   assert.notEqual(err, null);
-  assert.equal(err.code == -3008 || err.code == -3007, true);
 });
 
 // Test with non string hostname.

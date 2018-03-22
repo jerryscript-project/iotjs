d512f8b547d83b987f7b28cd16b0a462a44f781e
Hosung Kim <hs852.kim@samsung.com>
2018-03-22 13:40:14 +0900

    Implement tizen service app framework on IoT.js
    IoT.js-DCO-1.0-Signed-off-by: Hosung Kim hs852.kim@samsung.com

---
+++ src/platform/tizen/iotjs_tizen_service_app.h
@@ -0,0 +1,32 @@
+/* Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IOTJS_TIZEN_SERVICE_APP_H
+#define IOTJS_TIZEN_SERVICE_APP_H
+
+
+#ifdef __cplusplus
+#define IOTJS_EXTERN_C extern "C"
+#else /* !__cplusplus */
+#define IOTJS_EXTERN_C extern
+#endif /* !__cplusplus */
+
+
+IOTJS_EXTERN_C int iotjs_service_app_start(int argc, char** argv, char* js_path,
+                                           void* event_callbacks,
+                                           void* user_data);
+
+
+#endif /* IOTJS_TIZEN_SERVICE_APP_H */

8d71ae23deb9d38189835f98dec8ce25903bee2e
Hosung Kim <hs852.kim@samsung.com>
2018-03-28 16:56:15 +0900

    Add tizen iotjs application template
    This is the template used to create the iot.js app on Tizen Studio
    
    IoT.js-DCO-1.0-Signed-off-by: Hosung Kim hs852.kim@samsung.com

---
+++ config/tizen/template/IoTjsApp/project/inc/main.h
@@ -0,0 +1,12 @@
+#ifndef __$(appName)_H__
+#define __$(appName)_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "$(appName)"
+
+
+#endif /* __$(appName)_H__ */
---
+++ config/tizen/template/IoTjsApp/project/src/main.c
@@ -0,0 +1,66 @@
+#include <tizen.h>
+#include <service_app.h>
+#include <iotjs_tizen_service_app.h>
+#include "$(appName).h"
+
+
+bool service_app_create(void *data)
+{
+	// Todo: add your code here.
+	return true;
+}
+
+void service_app_terminate(void *data)
+{
+	// Todo: add your code here.
+	return;
+}
+
+void service_app_control(app_control_h app_control, void *data)
+{
+	// Todo: add your code here.
+	return;
+}
+
+static void
+service_app_lang_changed(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LANGUAGE_CHANGED*/
+	return;
+}
+
+static void
+service_app_region_changed(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_REGION_FORMAT_CHANGED*/
+}
+
+static void
+service_app_low_battery(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LOW_BATTERY*/
+}
+
+static void
+service_app_low_memory(app_event_info_h event_info, void *user_data)
+{
+	/*APP_EVENT_LOW_MEMORY*/
+}
+
+int main(int argc, char* argv[])
+{
+	char ad[50] = {0,};
+	service_app_lifecycle_callback_s event_callback;
+	app_event_handler_h handlers[5] = {NULL, };
+
+	event_callback.create = service_app_create;
+	event_callback.terminate = service_app_terminate;
+	event_callback.app_control = service_app_control;
+
+	service_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, service_app_low_battery, &ad);
+	service_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, service_app_low_memory, &ad);
+	service_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, service_app_lang_changed, &ad);
+	service_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, service_app_region_changed, &ad);
+
+	return iotjs_service_app_start(argc, argv, "index.js", &event_callback, ad);
+}

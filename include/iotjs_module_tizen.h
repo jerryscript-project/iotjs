25cea320d206a32a40b684bb6a15c69bbe9054d0
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2018-04-25 17:21:18 +0900

    Add calling js functions based on string parameters
    As written in iotjs_tizen_service_app.c, iot.js app for tizen is a
    hybrid one, which combines features of both native and js app.
    This commit provides a way to call  js function bound with tizen
    module in the app model.
    
    This also includes a patch for the bug in getResPath.
    
    IoT.js-DCO-1.0-Signed-off-by: Daeyeon Jeong daeyeon.jeong@samsung.com

---
+++ include/iotjs_module_tizen.h
@@ -0,0 +1,47 @@
+/* Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IOTJS_MODULE_TIZEN_H
+#define IOTJS_MODULE_TIZEN_H
+
+#ifndef IOTJS_EXTERN_C
+#ifdef __cplusplus
+#define IOTJS_EXTERN_C extern "C"
+#else /* !__cplusplus */
+#define IOTJS_EXTERN_C extern
+#endif /* !__cplusplus */
+#endif /* !IOTJS_EXTERN_C */
+
+#include <app_control.h>
+#include <string.h>
+
+typedef void (*user_callback_t)(int error, const char* data);
+
+IOTJS_EXTERN_C
+void iotjs_tizen_app_control_cb(app_control_h app_control, void* user_data);
+
+IOTJS_EXTERN_C
+int iotjs_tizen_bridge_native(const char* fn_name, unsigned fn_name_size,
+                              const char* message, unsigned message_size,
+                              user_callback_t cb);
+
+#define IOTJS_TIZEN_CALL_JFUNC(name, msg, cb)                              \
+  ({                                                                       \
+    if (name != NULL && (msg) != NULL)                                     \
+      iotjs_tizen_bridge_native(name, strlen(name), msg, strlen(msg), cb); \
+  })
+
+
+#endif /* IOTJS_MODULE_TIZEN_H */

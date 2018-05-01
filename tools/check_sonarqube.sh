cb9e4571480698d3a4def02cf485db643cb59738
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2018-05-02 03:13:35 +0900

    Add SonarQube analysis as a Travis CI task
    According to https://docs.travis-ci.com/user/sonarcloud/#Analysis-of-
    internal-pull-requests, travis-ci sonarclould addon cannot support
    external PRs. So we skip the analysis for the PRs, but enable it when
    each PR is landed onto master.
    
    IoT.js-DCO-1.0-Signed-off-by: Daeyeon Jeong daeyeon.jeong@samsung.com

---
+++ tools/check_sonarqube.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [[ -n "${TRAVIS_PULL_REQUEST_SLUG}" &&
+         "${TRAVIS_PULL_REQUEST_SLUG}" != "${TRAVIS_REPO_SLUG}" ]]; then
+  echo "Skip: The pull request from ${TRAVIS_PULL_REQUEST_SLUG} is an \
+  external one. It's not supported yet in Travis-CI";
+else
+  git fetch --unshallow;
+  build-wrapper-linux-x86-64 --out-dir bw-output ./tools/build.py;
+  sonar-scanner;
+fi

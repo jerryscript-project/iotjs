3be70a87e945a9a9892ee032b6c89b7a50f69d35
László Langó <llango.u-szeged@partner.samsung.com>
2018-06-12 15:08:26 +0200

    Add test for #1463 (#1682)
    The reported assertion was resolved in JerryScript. Fixes #1463.
    
    IoT.js-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

---
+++ test/run_pass/issue/issue-1463.js
@@ -0,0 +1,31 @@
+/* Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var assert = require('assert');
+
+function readfile(fileName) {
+  return new Promise(function(resolve, reject) {
+    throw function() {}(), Buffer;
+  });
+};
+
+try {
+  readfile(readfile).then(function(value) {
+    loadfi([], 0);
+  }).prototype(function(e) {});
+  assert(false);
+} catch (ex) {
+  assert(ex instanceof TypeError);
+}

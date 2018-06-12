429eb65dce168b5c74de1e1410594eb29ec68e22
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2018-06-12 02:38:53 +0200

    Update the used docker image version to iotjs/ubuntu:0.7 (#1678)
    It contains the newest version (7.25) of NuttX.
    
    IoT.js-DCO-1.0-Signed-off-by: Robert Sipka rsipka.uszeged@partner.samsung.com

--- tools/travis_script.py
+++ tools/travis_script.py
@@ -60,7 +60,7 @@ def run_docker():
     ex.check_run_cmd('docker', ['run', '-dit', '--privileged',
                      '--name', DOCKER_NAME, '-v',
                      '%s:%s' % (TRAVIS_BUILD_PATH, DOCKER_IOTJS_PATH),
-                     'iotjs/ubuntu:0.6'])
+                     'iotjs/ubuntu:0.7'])
 
 def exec_docker(cwd, cmd):
     exec_cmd = 'cd %s && ' % cwd + ' '.join(cmd)

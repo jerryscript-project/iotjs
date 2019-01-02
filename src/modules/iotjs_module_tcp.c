d8d657b3148ac3c760d68e5c698afb5ca95d6d8c
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2019-01-02 10:39:43 +0100

    Avoid possible null pointer dereferencing in tcp connection
    IoT.js-DCO-1.0-Signed-off-by: Robert Sipka rsipka.uszeged@partner.samsung.com

--- src/modules/iotjs_module_tcp.c
+++ src/modules/iotjs_module_tcp.c
@@ -184,8 +184,9 @@ static void OnConnection(uv_stream_t* handle, int status) {
         iotjs_jval_get_object_native_handle(jclient_tcp,
                                             &this_module_native_info);
 
-    int err = uv_accept(handle, (uv_stream_t*)client_handle);
-    if (err) {
+
+    if (client_handle == NULL ||
+        uv_accept(handle, (uv_stream_t*)client_handle)) {
       jerry_release_value(args[0]);
       return;
     }
